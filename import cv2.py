3c06d9edd83749c14ba54880a7c89d76a5c0471e
NaveenManepalli <86225990+NaveenManepalli@users.noreply.github.com>
2023-04-28 19:40:21 +0530

    Add files via upload

--- import cv2.py
+++ import cv2.py
@@ -1,16 +1,16 @@
 import cv2
 
-# Get the image number from user input
+
 n = input("Enter the image number:")
 
-# Load the image in grayscale
+
 img = cv2.imread(n + '.jpg', cv2.IMREAD_GRAYSCALE)
 
-# Apply Gaussian blur to the image
+
 img_blur = cv2.GaussianBlur(img, (5, 5), 0)
 
-# Apply the unsharp mask to the image
+
 img_usm = cv2.addWeighted(img, ((2.1)*1.02), img_blur, ((-1.4)*1.02), 0)
 
-# Save the enhanced image
+
 cv2.imwrite('enhanced_image.jpg', img_usm)
